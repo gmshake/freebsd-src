8273b996c4c385b0e933bdd278c33c3196fc8f0a
Zhenlei Huang <zlei.huang@gmail.com>
2021-08-18 12:42:04 +0800

    Calculate correct lle type

--- sys/ofed/drivers/infiniband/core/ib_addr.c
+++ sys/ofed/drivers/infiniband/core/ib_addr.c
@@ -45,6 +45,7 @@ __FBSDID("$FreeBSD$");
 #include <net/route.h>
 #include <net/route/nhop.h>
 #include <net/netevent.h>
+#include <net/if_llatbl.h>
 #include <rdma/ib_addr.h>
 #include <rdma/ib.h>
 
@@ -397,10 +398,9 @@ static int addr4_resolve(struct sockaddr_in *src_in,
 		bool is_gw = (nh->nh_flags & NHF_GATEWAY) != 0;
 		memset(edst, 0, MAX_ADDR_LEN);
 #ifdef INET6
-		/* XXX check nh->gw_sa.sa_family */
 		if (is_gw && nh->gw_sa.sa_family == AF_INET6)
-			error = nd6_resolve(ifp, is_gw, NULL, &nh->gw_sa, edst,
-			    NULL, NULL);
+			error = nd6_resolve(ifp, LLE_SF(AF_INET, is_gw), NULL,
+			    &nh->gw_sa, edst, NULL, NULL);
 		else
 #endif
 			error = arpresolve(ifp, is_gw, NULL, is_gw ?
@@ -592,7 +592,7 @@ static int addr6_resolve(struct sockaddr_in6 *src_in,
 	} else {
 		bool is_gw = (nh->nh_flags & NHF_GATEWAY) != 0;
 		memset(edst, 0, MAX_ADDR_LEN);
-		error = nd6_resolve(ifp, is_gw, NULL, is_gw ?
+		error = nd6_resolve(ifp, LLE_SF(AF_INET6, is_gw), NULL, is_gw ?
 		    &nh->gw_sa : (const struct sockaddr *)&dst_tmp,
 		    edst, NULL, NULL);
 		if (error != 0)
