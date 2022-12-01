7d4872c7ff6680eb4c78959f7de2de4e66ce4aa6
Zhenlei Huang <zlei@FreeBSD.org>
2022-12-01 22:24:57 +0800

    route_cache: Add route cache framework

---
+++ sys/net/route/route_cache.c
@@ -0,0 +1,182 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2022 Zhenlei Huang
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+#include "opt_inet.h"
+#include "opt_inet6.h"
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/sysctl.h>
+
+#include <sys/kernel.h>
+#include <sys/types.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+#include <sys/pcpu.h>
+#include <sys/queue.h>
+#include <sys/smp.h>
+#include <sys/socket.h>
+
+#include <net/ethernet.h>
+#include <net/if.h>
+#include <net/if_var.h>
+#include <net/if_dl.h>
+#include <net/if_llatbl.h>
+#include <net/vnet.h>
+#include <net/route.h>
+#include <net/route/nhop.h>
+#include <net/route/route_ctl.h>
+
+#include <netinet/in.h>
+#include <netinet6/in6_var.h>
+
+#include <net/route/route_cache.h>
+
+#include <vm/uma.h>
+
+static uma_zone_t route_cache_pcpu_zone;
+
+static void
+route_cache_pcpu_zone_init(void)
+{
+	route_cache_pcpu_zone = uma_zcreate("route-cache-pcpu",
+	    sizeof(struct route_cache_pcpu), NULL, NULL, NULL, NULL,
+	    UMA_ALIGN_PTR, UMA_ZONE_PCPU);
+}
+
+SYSINIT(route_cache_pcpu_zone_init, SI_SUB_PROTO_DOMAIN, SI_ORDER_FIRST,
+    route_cache_pcpu_zone_init, NULL);
+
+static void
+route_cache_pcpu_zone_uninit(void)
+{
+	uma_zdestroy(route_cache_pcpu_zone);
+}
+
+SYSUNINIT(route_cache_pcpu_zone_uninit, SI_SUB_PROTO_DOMAIN, SI_ORDER_ANY,
+    route_cache_pcpu_zone_uninit, NULL);
+
+void
+route_cache_init(struct route_cache *rc)
+{
+	int cpu;
+	struct route_cache_pcpu *pcpu;
+	struct route_cache_pcpu *rc_pcpu = uma_zalloc_pcpu(route_cache_pcpu_zone,
+	    M_WAITOK | M_ZERO);
+	CPU_FOREACH(cpu) {
+		pcpu = zpcpu_get_cpu(rc_pcpu, cpu);
+#ifdef INET
+		pcpu->ro.ro_flags = RT_LLE_CACHE; /* Cache L2 as well */
+#else
+#ifdef INET6
+		pcpu->ro6.ro_flags = RT_LLE_CACHE; /* Cache L2 as well */
+#endif
+#endif
+		mtx_init(&pcpu->mtx, "route_cache_pcpu_mtx", NULL, MTX_DEF);
+	}
+	rc->rc_pcpu = rc_pcpu;
+	rc->rs = NULL;
+}
+
+void
+route_cache_uninit(struct route_cache *rc)
+{
+	int cpu;
+	struct route_cache_pcpu *pcpu;
+	CPU_FOREACH(cpu) {
+		pcpu = zpcpu_get_cpu(rc->rc_pcpu, cpu);
+		mtx_assert(&pcpu->mtx, MA_NOTOWNED);
+#ifdef INVARIANTS
+		mtx_lock(&pcpu->mtx);
+#ifdef INET
+		KASSERT((pcpu->ro.ro_nh == NULL), ("route nexthop cache is not freed"));
+		KASSERT((pcpu->ro.ro_lle == NULL), ("route llentry is not freed"));
+#else
+#ifdef INET6
+		KASSERT((pcpu->ro6.ro_nh == NULL), ("route nexthop cache is not freed"));
+		KASSERT((pcpu->ro6.ro_lle == NULL), ("route llentry is not freed"));
+#endif
+#endif
+		mtx_unlock(&pcpu->mtx);
+#endif
+		mtx_destroy(&pcpu->mtx);
+	}
+	uma_zfree_pcpu(route_cache_pcpu_zone, rc->rc_pcpu);
+	rc->rc_pcpu = NULL; /* XXX 0xdeadc0de ? */
+}
+
+// XXX invalid cache via smp_rendezvous() ?
+void
+route_cache_invalidate(struct route_cache *rc)
+{
+	int cpu;
+	struct route_cache_pcpu *pcpu;
+	CPU_FOREACH(cpu) {
+		pcpu = zpcpu_get_cpu(rc->rc_pcpu, cpu);
+		mtx_lock(&pcpu->mtx);
+#ifdef INET
+		RO_INVALIDATE_CACHE(&pcpu->ro);
+#else
+#ifdef INET6
+		RO_INVALIDATE_CACHE(&pcpu->ro6);
+#endif
+#endif
+		mtx_unlock(&pcpu->mtx);
+	}
+}
+
+static void
+route_cache_subscription_cb(struct rib_head *rnh __unused,
+    struct rib_cmd_info *rci __unused, void *arg)
+{
+	struct route_cache *rc = arg;
+	route_cache_invalidate(rc);
+}
+
+void
+route_cache_subscribe_rib_event(struct route_cache *rc, int family,
+    uint32_t fibnum)
+{
+	KASSERT((rc->rs == NULL), ("already subscribed rib event"));
+	rc->rs = rib_subscribe(fibnum, family, route_cache_subscription_cb,
+	    rc, RIB_NOTIFY_IMMEDIATE, true);
+}
+
+void
+route_cache_unsubscribe_rib_event(struct route_cache *rc)
+{
+	struct epoch_tracker et;
+
+	if (rc->rs != NULL) {
+		NET_EPOCH_ENTER(et);
+		rib_unsubscribe(rc->rs);
+		NET_EPOCH_EXIT(et);
+		rc->rs = NULL;
+	}
+}
---
+++ sys/net/route/route_cache.h
@@ -0,0 +1,117 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2022 Zhenlei Huang
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _NET_ROUTE_ROUTE_CACHE_H_
+#define _NET_ROUTE_ROUTE_CACHE_H_
+
+#include "opt_inet.h"
+#include "opt_inet6.h"
+
+struct route_cache_pcpu;
+
+struct route_cache {
+	struct route_cache_pcpu *rc_pcpu;
+	struct rib_subscription *rs;
+};
+
+struct route_cache_pcpu {
+	struct mtx mtx;
+	union {
+#ifdef INET
+		struct route     ro;
+#endif
+#ifdef INET6
+		struct route_in6 ro6;
+#endif
+	};
+} __aligned(CACHE_LINE_SIZE);
+
+
+void route_cache_init(struct route_cache *);
+void route_cache_uninit(struct route_cache *);
+void route_cache_invalidate(struct route_cache *);
+
+void route_cache_subscribe_rib_event(struct route_cache *, int, uint32_t);
+void route_cache_unsubscribe_rib_event(struct route_cache *);
+
+#ifdef INET
+static inline struct route *
+route_cache_acquire(struct route_cache *rc)
+{
+	struct route_cache_pcpu *pcpu;
+	struct route *ro = NULL;
+
+	pcpu = zpcpu_get(rc->rc_pcpu);
+	if (mtx_trylock(&pcpu->mtx))
+		ro = &pcpu->ro;
+
+	return ro;
+}
+
+static inline void
+route_cache_release(struct route *ro)
+{
+	struct route_cache_pcpu *pcpu;
+
+	if (ro != NULL) {
+		pcpu = __containerof(ro, struct route_cache_pcpu, ro);
+		mtx_assert(&pcpu->mtx, MA_OWNED);
+		mtx_unlock(&pcpu->mtx);
+	}
+}
+#endif
+
+#ifdef INET6
+static inline struct route_in6 *
+route_cache_acquire6(struct route_cache *rc)
+{
+	struct route_cache_pcpu *pcpu;
+	struct route_in6 *ro = NULL;
+
+	pcpu = zpcpu_get(rc->rc_pcpu);
+	if (mtx_trylock(&pcpu->mtx))
+		ro = &pcpu->ro6;
+
+	return ro;
+}
+
+static inline void
+route_cache_release6(struct route_in6 *ro)
+{
+	struct route_cache_pcpu *pcpu;
+
+	if (ro != NULL) {
+		pcpu = __containerof(ro, struct route_cache_pcpu, ro6);
+		mtx_assert(&pcpu->mtx, MA_OWNED);
+		mtx_unlock(&pcpu->mtx);
+	}
+}
+#endif
+
+#endif
