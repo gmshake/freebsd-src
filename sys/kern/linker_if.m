ed32ee0494354301f24d7fea8e839b009a2aab2e
Zhenlei Huang <zlei@FreeBSD.org>
2023-04-17 22:16:08 +0800

    sysctl(9): Enable loader tunable for vnet sysctl variables
    Discussed with:	hselasky, glebius
    Fixes:		3da1cf1e88f8 Extend the meaning of the CTLFLAG_TUN flag ...
    MFC after:	2 weeks

--- sys/kern/linker_if.m
+++ sys/kern/linker_if.m
@@ -155,3 +155,12 @@ STATICMETHOD int link_preload {
 METHOD int link_preload_finish {
     linker_file_t	file;
 };
+
+#ifdef VIMAGE
+#
+# Propagate system tunable values to all vnet.
+#
+METHOD void propagate_vnets {
+	linker_file_t	file;
+};
+#endif
